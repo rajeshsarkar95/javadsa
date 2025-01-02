83ae6c009745303deeb08c520654a72fa995a648
rajesh <prorajeshsarkar@gmail.com>
2025-01-02 12:16:31 +0530

    bubbleshort

--- Main.java
+++ Main.java
@@ -1,5 +1,37 @@
-public class Main {
-     public static void main(){
-        System.out.println("hello  world");
-     }
+public class Main{
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Unsorted array:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        bubbleSort(numbers);
+
+        System.out.println("\nSorted array:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
 }
